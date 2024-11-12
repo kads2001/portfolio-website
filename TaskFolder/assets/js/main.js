beb7162c4972d8d12bf3706f55c3b059ca57d0d6
Evolutionco <adesh.yadav@evolutionco.com>
2024-11-12 11:27:16 +0530

    Qc ui fixes adesh

---
+++ TaskFolder/assets/js/main.js
@@ -0,0 +1,7 @@
+const menubtn = document.querySelector(".manu-hamber-par");
+const menubtnin = document.querySelector(".manu-hamber");
+const
+
+menubtn.addEventListener("click", function() {
+    menubtnin.classList.toggle("active"); // Adds or removes 'active' class to .manu-hamber
+});
